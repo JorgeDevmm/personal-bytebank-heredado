83e090328ac4fe514d6bb553b02c81170e75ae9a
Jorge Monzon <jorge29luis10@gmail.com>
2023-01-19 17:33:46 -0500

    proyecto finalizado 0.2

--- src/proyecto/CalculadoraDeImpuesto.java
+++ src/proyecto/CalculadoraDeImpuesto.java
@@ -4,6 +4,7 @@ public class CalculadoraDeImpuesto {
 
     private double totalImpuesto;
 
+    //este método pide como parametro un rotulo de interface y ese rotulo al esta implementado en dos clases, se pueden colocar cualquiera de los dos objetos
     public void registra(Tributacion t) {
         double valor = t.getvalorImpuesto();
         this.totalImpuesto += valor;
